92a24dea6f865cf85c59d0ff343fad5e04a3db59
Adam <diamond.tiger911@gmail.com>
2023-09-27 00:15:42 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import './globals.css'
+import type { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'JS Mastery ',
+  description: 'JS Mastery Resources',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className='min-h-screen bg-black font-Poppins'>{children}</body>
+    </html>
+  )
+}
